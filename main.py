6401f792bcdfcb65b6aa9723fc9012179f809118
Mohammad Amin Dadkhah <mohammadamin.dadkhah13@gmail.com>
2025-07-08 19:32:41 +0330

    test

--- main.py
+++ main.py
@@ -40,3 +40,5 @@ match site:
 if products:
     with open(f"data/{keyword}.json", "a+", encoding="utf-8") as f:
         json.dump(products, f, ensure_ascii=False, indent=4)
+else:
+    print("No products found.")
